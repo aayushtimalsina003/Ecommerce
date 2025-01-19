928cf2d0ad598cb6672af977ecfde3bc84b5300a
Aayush Timalsina <aayushtimalsina2@gmail.com>
2025-01-19 19:42:00 +0545

    Add Redux setup with counter functionality and integrate into ContactPage

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2979f40d350a8cdeb3240e0b2088da3bd46edb08")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8137e1fcdf55ab551a8474c7d13df14301c2dd4d")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- client/src/features/contact/ContactPage.tsx
+++ client/src/features/contact/ContactPage.tsx
@@ -1,5 +1,26 @@
+import { useDispatch, useSelector } from "react-redux";
+import { CounterState, decrementLegacy, incrementLegacy } from "./contactReducer";
+import { Button, ButtonGroup, Typography } from "@mui/material";
+
 export default function ContactPage() {
+  const data = useSelector((state: CounterState) => state.data);
+  const dispatch = useDispatch();
+
   return (
-    <div>ContactPage</div>
-  )
+    <>
+      <Typography variant="h2">Contact Page</Typography>
+      <Typography variant="body1">The data is : {data}</Typography>
+      <ButtonGroup>
+        <Button onClick={() => dispatch(decrement())} color="error">
+          Decrement
+        </Button>
+        <Button onClick={() => dispatch(increment())} color="success">
+          Increment
+        </Button>
+        <Button onClick={() => dispatch(increment(5))} color="secondary">
+          Increment by 5
+        </Button>
+      </ButtonGroup>
+    </>
+  );
 }
