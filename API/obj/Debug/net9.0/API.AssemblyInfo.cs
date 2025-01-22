8581b852461e681c9d5d092f6e439a59ef17fdbf
Aayush Timalsina <aayushtimalsina2@gmail.com>
2025-01-22 13:19:04 +0545

    Add error handling pages for server errors and not found; update routing to include new error components

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+060397996df72756f5b313882d0ce2f3b96868e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f895e5fd2c1afcbbdfdf5fb2490f73495043e552")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- client/src/app/routes/Routes.tsx
+++ client/src/app/routes/Routes.tsx
@@ -1,21 +1,26 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, Navigate } from "react-router-dom";
 import App from "../layout/App";
 import HomePage from "../../features/home/HomePage";
 import ProductDetails from "../../features/catelog/ProductDetails";
 import AboutPage from "../../features/about/AboutPage";
 import ContactPage from "../../features/contact/ContactPage";
 import Catelog from "../../features/catelog/Catelog";
+import ServerError from "../errors/ServerError";
+import NotFound from "../errors/NotFound";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
     children: [
-        { path: "", element: <HomePage /> },
-        { path: "/catelog", element: <Catelog /> },
-        { path: "/catelog/:id", element: <ProductDetails /> },
-        { path: "/about", element: <AboutPage /> },
-        { path: "/contact", element: <ContactPage /> },
+      { path: "", element: <HomePage /> },
+      { path: "/catelog", element: <Catelog /> },
+      { path: "/catelog/:id", element: <ProductDetails /> },
+      { path: "/about", element: <AboutPage /> },
+      { path: "/contact", element: <ContactPage /> },
+      { path: "/server-error", element: <ServerError /> },
+      { path: "/not-found", element: <NotFound /> },
+      { path: "*", element: <Navigate replace to="/not-found" /> },
     ],
   },
 ]);
