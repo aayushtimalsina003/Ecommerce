42f1dc54ee820e8c21deeb59a4720fcd8cc24e68
Aayush Timalsina <aayushtimalsina2@gmail.com>
2025-02-03 22:07:32 +0545

    Add Order aggregate entities and update database context; implement order status, payment summary, and shipping address

---
+++ API/Controllers/OrdersController.cs
@@ -0,0 +1,105 @@
+using API.Data;
+using API.DTOs;
+using API.Entities;
+using API.Entities.OrderAggregate;
+using API.Extensions;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+public class OrdersController(StoreContext context) : BaseApiController
+{
+    [Authorize]
+    [HttpGet]
+    public async Task<ActionResult<List<Order>>> GetOrders()
+    {
+        var orders = await context.Orders
+            .Include(x => x.OrderItems)
+            .Where(x => x.BuyerEmail == User.GetUsername())
+            .ToListAsync();
+        return orders;
+    }
+
+    [HttpGet("{id:int}")]
+    public async Task<ActionResult<Order>> GetOrderDetails(int id)
+    {
+        var order = await context.Orders
+            .Where(x => x.BuyerEmail == User.GetUsername() && id == x.Id)
+            .FirstOrDefaultAsync();
+
+        if (order == null) return NotFound();
+
+        return order;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Order>> CreateOrder(CreateOrderDto orderDto)
+    {
+        var basket = await context.Baskets.GetBasketWithItems(Request.Cookies["basketId"]);
+
+        if (basket == null || basket.Items.Count == 0 || string.IsNullOrEmpty(basket.PaymentIntentId))
+            return BadRequest("Basket is empty or not found");
+
+        var items = CreateOrderItems(basket.Items);
+        if (items == null) return BadRequest("Some items are out of stock.");
+
+        var subtotal = items.Sum(x => x.Price * x.Quantity);
+        var deliveryFee = CalculateDeliveryFee(subtotal);
+
+        var order = new Order
+        {
+            OrderItems = items,
+            BuyerEmail = User.GetUsername(),
+            ShippingAddress = orderDto.ShippingAddress,
+            DeliveryFee = deliveryFee,
+            Subtotal = subtotal,
+            PaymentSummary = orderDto.PaymentSummary,
+            PaymentIntentId = basket.PaymentIntentId,
+        };
+
+        context.Orders.Add(order);
+
+        context.Baskets.Remove(basket);
+        Response.Cookies.Delete("basketId");
+
+        var request = await context.SaveChangesAsync() > 0;
+
+        if (!request) return BadRequest("Problem creating order");
+
+        return CreatedAtAction(nameof(GetOrderDetails), new { id = order.Id }, order);
+    }
+
+    private long CalculateDeliveryFee(long subtotal)
+    {
+        return subtotal > 10000 ? 0 : 500;
+    }
+
+    private List<OrderItem>? CreateOrderItems(List<BasketItem> items)
+    {
+        var orderItems = new List<OrderItem>();
+        foreach (var item in items)
+        {
+            if (item.Product.QuantityInStock < item.Quantity)
+                return null;
+
+            var orderItem = new OrderItem
+            {
+                ItemOrdered = new ProductItemOrdered
+                {
+                    ProductId = item.ProductId,
+                    PictureUrl = item.Product.PictureUrl,
+                    Name = item.Product.Name
+                },
+                Price = item.Product.Price,
+                Quantity = item.Quantity
+            };
+            orderItems.Add(orderItem);
+
+            item.Product.QuantityInStock -= item.Quantity;
+        }
+
+        return orderItems;
+    }
+}
